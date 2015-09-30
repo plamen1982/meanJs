bc4e621709ae20554dfb2ec77eb50cb134c684c5
plamen1982 <plamen.hristov@ymail.com>
2015-09-30 18:32:22 -0400

    Inject toastr.js into the app, create custome service notifier

--- public/app/account/LoginCtrl.js
+++ public/app/account/LoginCtrl.js
@@ -1,7 +1,11 @@
-app.controller('LoginCtrl', function($scope, $http){
+app.controller('LoginCtrl', function($scope, $http, notifier){
   $scope.login =  function(user){
-        $http.post('/login', user).then(function(res){
-            console.log(res)
+        $http.post('/login', user).then(function(data){
+            if(data.success){
+            	notifier.success('Successful login')
+            } else{
+            	notifier.error('Username/Password combination is not valid')
+            }
         })
     }
 });
